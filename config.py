7b8dbfff3fd1587c7369e4822c435d6cf655ecb6
IsoSportEnjoyer73 <nikola.dr125@gmail.com>
2026-02-08 19:34:56 +0200

    Good practice .env changed config

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Config:
-    SECRET_KEY: str = os.environ.get('SECRET_KEY') or 'dev-secret-key'
-    SQLALCHEMY_DATABASE_URI: str = 'sqlite:///reservations.db'
+    SECRET_KEY: str = os.environ.get('SECRET_KEY', 'dev-secret-key')
+    SQLALCHEMY_DATABASE_URI: str = os.environ.get('DATABASE_URL', 'sqlite:///reservations.db')
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
+    DEBUG: bool = os.environ.get('FLASK_DEBUG', '0') == '1'
